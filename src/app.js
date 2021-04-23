07d68568387fbd5abcb387b19bd5fdd19bcbb9b3
Letícia Lima <leticialimaferreira2@gmail.com>
2021-04-23 16:07:40 -0300

    repository, conexão com mongodb

--- src/app.js
+++ src/app.js
@@ -1,4 +1,12 @@
 const express = require('express');
 const app = express();
+const cors = require("cors");
+const db = require('./model/repository');
+
+app.use(cors());
+app.use(express.json());
+db.connect();
+
+// app.use("/");
 
 module.exports = app
