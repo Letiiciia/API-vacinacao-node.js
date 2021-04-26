5063a9eb3dfce65190a297fa64b9657aa2dfcb92
Letícia Lima <leticialimaferreira2@gmail.com>
2021-04-25 21:24:20 -0300

    metodo delete

--- src/controller/cadastro-vacinaController.js
+++ src/controller/cadastro-vacinaController.js
@@ -39,7 +39,26 @@ const criarCadastro = (request, response) => {
     })
 }
 
+const deleteCadastro = (request, response) => {
+    console.log(request.url);
+    const id = request.params.id;
+    
+    vacinaCollection.vacinaCollection.findByIdAndDelete({_id:id}, (error) => {
+        if(error){
+            return response.status(400).send({
+                message: "Falha ao deletar",
+                error
+            })
+        }else{
+            return response.status(200).send({
+                message: "Deletado com sucesso - " + id,
+            })
+        }
+    })
+}
+
 module.exports = {
     getCadastrosVacina,
-    criarCadastro
+    criarCadastro,
+    deleteCadastro
 }
--- src/router/cadastro-vacinaRoutes.js
+++ src/router/cadastro-vacinaRoutes.js
@@ -4,6 +4,7 @@ const cadastroController = require('../controller/cadastro-vacinaController');
 
 router.get("/cadastros", cadastroController.getCadastrosVacina);
 router.post("/criar", cadastroController.criarCadastro);
+router.delete("/delete/:id", cadastroController.deleteCadastro);
 
 
 
